a96bc1b0da7eb0674744522082e670bc01855c7f
Tim Hutton <tim.hutton@gmail.com>
2016-06-15 00:01:54 +0100

    Fix: start xterm in background. Minor: added file comment.

--- Malmo/launch_minecraft_in_background.py
+++ Malmo/launch_minecraft_in_background.py
@@ -1,3 +1,5 @@
+# Used for integration tests. (To launch Minecraft normally, see launchClient in the Minecraft folder.)
+
 import os
 import socket
 import subprocess
@@ -22,7 +24,7 @@ path = os.path.join( os.getcwd(), "..", "Minecraft" )
 if os.name == 'nt':
     subprocess.call( "start cmd /C \"cd " + path + " && launchClient -port 10000\"", shell=True )
 else:
-    subprocess.call( "xterm -e \"cd " + path + " && ./launchClient.sh -port 10000\"", shell=True )
+    subprocess.call( "xterm -e \"cd " + path + " && ./launchClient.sh -port 10000\" &", shell=True )
 
 print 'Giving Minecraft some time to launch: ',
 for i in xrange( 100 ):
