097df8a5249d2b40659fcd8bed65c2f8f9512b8d
Tim Hutton <tim.hutton@gmail.com>
2016-06-14 23:44:19 +0100

    Fix: need shell=True on Linux too.

--- Malmo/launch_minecraft_in_background.py
+++ Malmo/launch_minecraft_in_background.py
@@ -1,7 +1,10 @@
 import os
 import socket
 import subprocess
+import sys
 import time
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
 def PortHasListener( port ):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,7 +22,7 @@ path = os.path.join( os.getcwd(), "..", "Minecraft" )
 if os.name == 'nt':
     subprocess.call( "start cmd /C \"cd " + path + " && launchClient -port 10000\"", shell=True )
 else:
-    subprocess.call( "xterm -e \"cd " + path + " && ./launchClient.sh -port 10000\"" )
+    subprocess.call( "xterm -e \"cd " + path + " && ./launchClient.sh -port 10000\"", shell=True )
 
 print 'Giving Minecraft some time to launch: ',
 for i in xrange( 100 ):
