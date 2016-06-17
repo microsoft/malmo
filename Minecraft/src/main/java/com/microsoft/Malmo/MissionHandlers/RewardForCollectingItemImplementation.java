ff06bed95864649b7c2d645ea729c723d1d82f26
davey <a-dabign@microsoft.com>
2016-06-17 17:44:43 +0100

    Indentation.

--- Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/RewardForCollectingItemImplementation.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/RewardForCollectingItemImplementation.java
@@ -12,36 +12,36 @@ import com.microsoft.Malmo.Schemas.RewardForCollectingItem;
 
 public class RewardForCollectingItemImplementation extends RewardForItemBase implements IRewardProducer
 {
-	@Override
-	public boolean parseParameters(Object params)
-	{
-		if (params == null || !(params instanceof RewardForCollectingItem))
-			return false;
+    @Override
+    public boolean parseParameters(Object params)
+    {
+        if (params == null || !(params instanceof RewardForCollectingItem))
+            return false;
 
-		// Build up a map of rewards per item:
-		RewardForCollectingItem rciparams = (RewardForCollectingItem)params;
-		for (ItemSpec is : rciparams.getItem())
-		    addItemSpecToRewardStructure(is);
+        // Build up a map of rewards per item:
+        RewardForCollectingItem rciparams = (RewardForCollectingItem)params;
+        for (ItemSpec is : rciparams.getItem())
+            addItemSpecToRewardStructure(is);
 
-		return true;
-	}
+        return true;
+    }
 
-	@SubscribeEvent
-	public void onPickupItem(EntityItemPickupEvent event)
-	{
-		if (event.item != null && event.item.getEntityItem() != null)
-		{
-			ItemStack stack = event.item.getEntityItem();
-			accumulateReward(stack);
-		}
-	}
-	
-	@Override
-	public float getReward(MissionInit missionInit)
-	{
-	    return getReward();
-	}
-	
+    @SubscribeEvent
+    public void onPickupItem(EntityItemPickupEvent event)
+    {
+        if (event.item != null && event.item.getEntityItem() != null)
+        {
+            ItemStack stack = event.item.getEntityItem();
+            accumulateReward(stack);
+        }
+    }
+
+    @Override
+    public float getReward(MissionInit missionInit)
+    {
+        return getReward();
+    }
+
     @Override
     public void prepare(MissionInit missionInit)
     {
--- Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/RewardForDiscardingItemImplementation.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/RewardForDiscardingItemImplementation.java
@@ -8,41 +8,40 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import com.microsoft.Malmo.MissionHandlerInterfaces.IRewardProducer;
 import com.microsoft.Malmo.Schemas.ItemSpec;
 import com.microsoft.Malmo.Schemas.MissionInit;
-import com.microsoft.Malmo.Schemas.RewardForCollectingItem;
 import com.microsoft.Malmo.Schemas.RewardForDiscardingItem;
 
 public class RewardForDiscardingItemImplementation extends RewardForItemBase implements IRewardProducer
 {
-	@Override
-	public boolean parseParameters(Object params)
-	{
-		if (params == null || !(params instanceof RewardForDiscardingItem))
-			return false;
+    @Override
+    public boolean parseParameters(Object params)
+    {
+        if (params == null || !(params instanceof RewardForDiscardingItem))
+            return false;
 
-		// Build up a map of rewards per item:
-		RewardForDiscardingItem rdiparams = (RewardForDiscardingItem)params;
-		for (ItemSpec is : rdiparams.getItem())
-		    addItemSpecToRewardStructure(is);
+        // Build up a map of rewards per item:
+        RewardForDiscardingItem rdiparams = (RewardForDiscardingItem)params;
+        for (ItemSpec is : rdiparams.getItem())
+            addItemSpecToRewardStructure(is);
 
-		return true;
-	}
+        return true;
+    }
 
-	@SubscribeEvent
-	public void onTossItem(ItemTossEvent event)
-	{
-	    if (event.entityItem != null)
-	    {
-	        ItemStack stack = event.entityItem.getEntityItem();
-	        accumulateReward(stack);
-	    }
-	}
+    @SubscribeEvent
+    public void onTossItem(ItemTossEvent event)
+    {
+        if (event.entityItem != null)
+        {
+            ItemStack stack = event.entityItem.getEntityItem();
+            accumulateReward(stack);
+        }
+    }
 
-	@Override
-	public float getReward(MissionInit missionInit)
-	{
-	    return getReward();
-	}
-	
+    @Override
+    public float getReward(MissionInit missionInit)
+    {
+        return getReward();
+    }
+
     @Override
     public void prepare(MissionInit missionInit)
     {
