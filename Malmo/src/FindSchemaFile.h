d8ce760acdc1007800551769312da14cbfe5b226
Tim Hutton <a-tihutt@microsoft.com>
2016-06-22 17:32:18 +0100

    Minor: updated FindSchemaFile comment.

--- Malmo/src/FindSchemaFile.h
+++ Malmo/src/FindSchemaFile.h
@@ -11,7 +11,7 @@
 namespace malmo {
 
     // Use the MALMO_XSD_PATH environment variable, if set, to find a .xsd file.
-    // Or look in the current directory. If not found, throw an exception.
+    // Or look in the current directory. Or in ../Schemas. If not found, throw an exception.
     std::string FindSchemaFile( const std::string& name );
 }
 
