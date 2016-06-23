e3644274229f386267b7d459785705eabf3e5294
Tim Hutton <tim.hutton@gmail.com>
2016-06-23 21:32:55 +0100

    Fix: Travis-CI Ubuntu build needs MALMO_XSD_PATH set

--- .travis/ci_ubuntu14.04.sh
+++ .travis/ci_ubuntu14.04.sh
@@ -46,6 +46,7 @@ make
 
 # git clone <MalmoURL> $TRAVIS_BUILD_DIR  (our CI environment does this for us)
 wget https://raw.githubusercontent.com/bitfehler/xs3p/master/xs3p.xsl -P $TRAVIS_BUILD_DIR/Schemas
+export MALMO_XSD_PATH=$TRAVIS_BUILD_DIR/Schemas
 cd $TRAVIS_BUILD_DIR
 mkdir build
 cd build
