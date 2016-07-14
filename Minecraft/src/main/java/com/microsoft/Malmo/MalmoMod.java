c11dfa23f2622529598cb00758f9c6a153727a83
davey <a-dabign@microsoft.com>
2016-07-14 14:10:00 +0100

    Moved inventory commands to server-side, added combine command.

--- Minecraft/src/main/java/com/microsoft/Malmo/MalmoMod.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/MalmoMod.java
@@ -53,6 +53,7 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import com.microsoft.Malmo.Client.MalmoModClient;
 import com.microsoft.Malmo.MissionHandlers.AbsoluteMovementCommandsImplementation;
+import com.microsoft.Malmo.MissionHandlers.InventoryCommandsImplementation;
 import com.microsoft.Malmo.MissionHandlers.ObservationFromFullStatsImplementation;
 import com.microsoft.Malmo.MissionHandlers.ObservationFromGridImplementation;
 import com.microsoft.Malmo.MissionHandlers.SimpleCraftCommandsImplementation;
@@ -116,6 +117,7 @@ public class MalmoMod
         network.registerMessage(SimpleCraftCommandsImplementation.CraftMessageHandler.class, SimpleCraftCommandsImplementation.CraftMessage.class, 4, Side.SERVER);
         network.registerMessage(AbsoluteMovementCommandsImplementation.TeleportMessageHandler.class, AbsoluteMovementCommandsImplementation.TeleportMessage.class, 5, Side.SERVER);
         network.registerMessage(MalmoMessageHandler.class, MalmoMessage.class, 6, Side.SERVER);	// Malmo messages from client to server
+        network.registerMessage(InventoryCommandsImplementation.InventoryMessageHandler.class, InventoryCommandsImplementation.InventoryMessage.class, 7, Side.SERVER);
     }
 
     public Configuration getModSessionConfigFile() { return this.sessionConfig; }
