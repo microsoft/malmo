7d8b55d6c755c2ac628cc1ca71d42662b5d478f3
Tim Hutton <a-tihutt@microsoft.com>
2016-07-19 19:36:41 +0100

    Added buttons to canvas. Mapping mouse to turn and pitch (work in progress).

--- Malmo/samples/Python_examples/human_action.py
+++ Malmo/samples/Python_examples/human_action.py
@@ -23,14 +23,124 @@ import MalmoPython
 import os
 import sys
 import time
-import Tkinter as tk
+from Tkinter import *
 from PIL import Image
 from PIL import ImageTk
 
-root = tk.Tk()
-root.wm_title("Video Output")
-canvas = tk.Canvas(root, width=320, height=200, borderwidth=0, highlightthickness=0, bg="white")
-canvas.pack()
+prev_x = prev_y = 0
+
+def onStartMission():
+    print 'starting mission'
+    my_mission_record_spec = MalmoPython.MissionRecordSpec()
+    agent_host.startMission( my_mission, my_mission_record_spec )
+
+    print "Waiting for the mission to start",
+    world_state = agent_host.peekWorldState()
+    while not world_state.is_mission_running:
+        sys.stdout.write(".")
+        time.sleep(0.1)
+        world_state = agent_host.peekWorldState()
+        for error in world_state.errors:
+            print "Error:",error.text
+    print
+
+    while world_state.is_mission_running:
+        if world_state.number_of_video_frames_since_last_state > 0:
+            frame = world_state.video_frames[-1]
+            buff = buffer(frame.pixels, 0, frame.width * frame.height * frame.channels)
+            image = Image.frombytes('RGB', (frame.width,frame.height), buff)
+            photo = ImageTk.PhotoImage(image)
+            canvas.delete("all")
+            canvas.create_image(frame.width/2, frame.height/2, image=photo)
+            root.update()
+        time.sleep(0.01)
+        world_state = agent_host.getWorldState()
+    print 'Mission has stopped.'
+    
+def onSendCommand():
+    agent_host.sendCommand(command_entry.get())
+    command_entry.delete(0,END)
+
+def onMouseMoveInCanvas(event):
+    canvas.focus_set()
+    global prev_x,prev_y
+    # TODO: still not quite right
+    turn_speed = ( event.x - prev_x ) / 10.0
+    agent_host.sendCommand( 'turn '+str(turn_speed) )
+    pitch_speed = ( event.y - prev_y ) / 10.0
+    agent_host.sendCommand( 'pitch '+str(pitch_speed) )
+    prev_x = event.x
+    prev_y = event.y
+    # TODO: this doesn't feel right - want trackball not flight or something
+    #agent_host.sendCommand( 'turn '+str( 2.0 * event.x / canvas.winfo_width() - 1 ) )
+    #agent_host.sendCommand( 'pitch '+str( 2.0 * event.y / canvas.winfo_height() - 1 ) )
+  
+def onLeftMouseDownInCanvas(event):
+    canvas.focus_set()
+    agent_host.sendCommand( 'attack 1' )
+  
+def onLeftMouseUpInCanvas(event):
+    canvas.focus_set()
+    agent_host.sendCommand( 'attack 0' )
+  
+def onRightMouseDownInCanvas(event):
+    canvas.focus_set()
+    agent_host.sendCommand( 'use 1' )
+  
+def onRightMouseUpInCanvas(event):
+    canvas.focus_set()
+    agent_host.sendCommand( 'use 0' )
+  
+def onKeyInCommandEntry(event):
+    if event.char == '\r':
+        onSendCommand()
+       
+def onKeyPressInCanvas(event):
+    print 'canvas key:',event.char
+    if event.char == 'w':
+        agent_host.sendCommand('move 1')
+    elif event.char == 'a':
+        agent_host.sendCommand('strafe -1')
+    elif event.char == 's':
+        agent_host.sendCommand('move -1')
+    elif event.char == 'd':
+        agent_host.sendCommand('strafe 1')
+    elif event.char == ' ':
+        agent_host.sendCommand('jump 1')
+
+def onKeyReleaseInCanvas(event):
+    print 'canvas key released:',event.char
+    if event.char == 'w':
+        agent_host.sendCommand('move 0')
+    elif event.char == 'a':
+        agent_host.sendCommand('strafe 0')
+    elif event.char == 's':
+        agent_host.sendCommand('move 0')
+    elif event.char == 'd':
+        agent_host.sendCommand('strafe 0')
+    elif event.char == ' ':
+        agent_host.sendCommand('jump 0')
+
+root = Tk()
+root.wm_title("Human Action Component")
+start_button = Button(root, text='Start', command=onStartMission,font = "Helvetica 36 bold")
+start_button.pack(padx=5, pady=5)
+canvas = Canvas(root, width=320, height=200, borderwidth=0, highlightthickness=0, bg="white" )
+canvas.focus_set()
+canvas.bind('<Motion>',onMouseMoveInCanvas)
+canvas.bind('<Button-1>',onLeftMouseDownInCanvas)
+canvas.bind('<ButtonRelease-1>',onLeftMouseUpInCanvas)
+canvas.bind('<Button-3>',onRightMouseDownInCanvas)
+canvas.bind('<ButtonRelease-3>',onRightMouseUpInCanvas)
+canvas.bind('<KeyPress>',onKeyPressInCanvas)
+canvas.bind('<KeyRelease>',onKeyReleaseInCanvas)
+#canvas.config(cursor='none')
+canvas.pack(padx=5, pady=5)
+Label(root, text="Command:",font = "Helvetica 16 bold").pack(padx=5, pady=5, side=LEFT)
+command_entry = Entry(root,font = "Helvetica 16 bold")
+command_entry.bind('<Key>',onKeyInCommandEntry)
+command_entry.pack(padx=5, pady=5, side=LEFT)
+Button(root, text='Send', command=onSendCommand,font = "Helvetica 16 bold").pack(padx=5, pady=5, side=LEFT)
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
@@ -47,31 +157,7 @@ if agent_host.receivedArgument("help"):
     
 my_mission = MalmoPython.MissionSpec()
 my_mission.requestVideo(800,600)
+my_mission.timeLimitInSeconds(20)
 canvas.config( width=800, height=600 )
-my_mission_record_spec = MalmoPython.MissionRecordSpec()
-agent_host.startMission( my_mission, my_mission_record_spec )
 
-print "Waiting for the mission to start",
-world_state = agent_host.peekWorldState()
-while not world_state.is_mission_running:
-    sys.stdout.write(".")
-    time.sleep(0.1)
-    world_state = agent_host.peekWorldState()
-    for error in world_state.errors:
-        print "Error:",error.text
-print
-
-agent_host.sendCommand('move 1')
-
-while world_state.is_mission_running:
-    if world_state.number_of_video_frames_since_last_state > 0:
-        frame = world_state.video_frames[-1]
-        buff = buffer(frame.pixels, 0, frame.width * frame.height * frame.channels)
-        image = Image.frombytes('RGB', (frame.width,frame.height), buff)
-        photo = ImageTk.PhotoImage(image)
-        canvas.delete("all")
-        canvas.create_image(frame.width/2, frame.height/2, image=photo)
-        root.update()
-    time.sleep(0.01)
-    world_state = agent_host.getWorldState()
-print 'Mission has stopped.'
+root.mainloop()
