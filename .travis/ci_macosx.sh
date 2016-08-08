4619869f690c1d2050c9117a2b9f436fda9b886d
Tim Hutton <a-tihutt@microsoft.com>
2016-08-08 16:02:59 +0100

    Minor: removed comments.

--- .travis/ci_macosx.sh
+++ .travis/ci_macosx.sh
@@ -8,12 +8,7 @@ set -o pipefail
 # install dependencies:
 
 brew update
-# echo Installing boost --with-python (removed, think causing problems on new Travis image)
-# brew install boost --with-python
-echo Installing boost-python
-brew install boost-python
-echo Installing other things
-brew install ffmpeg swig xerces-c doxygen
+brew install boost-python ffmpeg swig xerces-c doxygen
 sudo brew cask install java
 
 # mono and CodeSynthesis XSD both contain 'xsd' executables and we want the CodeSynthesis
