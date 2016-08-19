e6b8fad2129028a617eb192f58ae952589d619c3
Tim Hutton <a-tihutt@microsoft.com>
2016-08-19 11:55:57 +0100

    Working version of robust_frames.py for discrete_absolute movements.

--- Malmo/samples/Python_examples/robust_frames.py
+++ Malmo/samples/Python_examples/robust_frames.py
@@ -36,9 +36,17 @@ if save_images:
     
 class RandomAgent:
 
-    def __init__(self, agent_host ):
+    def __init__(self, agent_host, action_set ):
         self.rep = 0
         self.agent_host = agent_host
+        self.action_set = action_set
+        self.tolerance = 0.01
+        if action_set == 'discrete_absolute':
+            self.actions = ['movenorth 1', 'movesouth 1', 'movewest 1', 'moveeast 1']
+            self.require_move = True
+        else:
+            print 'ERROR: Unsupported action set:',action_set
+            exit(1)
 
     def waitForInitialState( self ):
         # wait for a valid observation
@@ -50,33 +58,28 @@ class RandomAgent:
         while world_state.is_mission_running and world_state.number_of_video_frames_since_last_state == num_frames_seen:
             world_state = self.agent_host.peekWorldState()
         world_state = self.agent_host.getWorldState()
-        for err in world_state.errors:
-            print err
 
-        if not world_state.is_mission_running:
-            return 0 # mission already ended
+        if world_state.is_mission_running:
+                
+            assert len(world_state.video_frames) > 0, 'No video frames!?'
             
-        assert len(world_state.video_frames) > 0, 'No video frames!?'
-        
-        obs = json.loads( world_state.observations[-1].text )
-        prev_x = obs[u'XPos']
-        prev_z = obs[u'ZPos']
-        print 'Initial position:',prev_x,',',prev_z
-        
-        if save_images:
-            # save the frame, for debugging
-            frame = world_state.video_frames[-1]
-            image = Image.frombytes('RGB', (frame.width, frame.height), str(frame.pixels) )
-            self.iFrame = 0
-            self.rep = self.rep + 1
-            image.save( 'rep_' + str(self.rep).zfill(3) + '_saved_frame_' + str(iFrame).zfill(4) + '.png' )
+            obs = json.loads( world_state.observations[-1].text )
+            self.prev_x = obs[u'XPos']
+            self.prev_z = obs[u'ZPos']
+            print 'Initial position:',self.prev_x,',',self.prev_z
+            
+            if save_images:
+                # save the frame, for debugging
+                frame = world_state.video_frames[-1]
+                image = Image.frombytes('RGB', (frame.width, frame.height), str(frame.pixels) )
+                self.iFrame = 0
+                self.rep = self.rep + 1
+                image.save( 'rep_' + str(self.rep).zfill(3) + '_saved_frame_' + str(iFrame).zfill(4) + '.png' )
             
         return world_state
 
-    def waitForNextState( self ):            
-        require_move = True
-        check_expected_position = True
-        
+    def waitForNextState( self ):
+       
         # wait for the position to have changed and a reward received
         print 'Waiting for data...',
         while True:
@@ -86,10 +89,10 @@ class RandomAgent:
                 break
             if len(world_state.rewards) > 0 and not all(e.text=='{}' for e in world_state.observations):
                 obs = json.loads( world_state.observations[-1].text )
-                curr_x = obs[u'XPos']
-                curr_z = obs[u'ZPos']
-                if require_move:
-                    if math.hypot( curr_x - prev_x, curr_z - prev_z ) > tol:
+                self.curr_x = obs[u'XPos']
+                self.curr_z = obs[u'ZPos']
+                if self.require_move:
+                    if math.hypot( self.curr_x - self.prev_x, self.curr_z - self.prev_z ) > self.tolerance:
                         print 'received.'
                         break
                 else:
@@ -101,10 +104,7 @@ class RandomAgent:
             world_state = self.agent_host.peekWorldState()
             
         num_frames_before_get = len(world_state.video_frames)
-        
         world_state = self.agent_host.getWorldState()
-        for err in world_state.errors:
-            print err
 
         if save_images:
             # save the frame, for debugging
@@ -121,41 +121,41 @@ class RandomAgent:
             assert num_frames_after_get >= num_frames_before_get, 'Fewer frames after getWorldState!?'
             frame = world_state.video_frames[-1]
             obs = json.loads( world_state.observations[-1].text )
-            curr_x = obs[u'XPos']
-            curr_z = obs[u'ZPos']
-            print 'New position from observation:',curr_x,',',curr_z,'after action:',self.actions[self.prev_a], #NSWE
-            if check_expected_position:
-                expected_x = prev_x + [0,0,-1,1][self.prev_a]
-                expected_z = prev_z + [-1,1,0,0][self.prev_a]
-                if math.hypot( curr_x - expected_x, curr_z - expected_z ) > tol:
-                    print ' - ERROR DETECTED! Expected:',expected_x,',',expected_z
-                    raw_input("Press Enter to continue...")
-                else:
-                    print 'as expected.'
-                curr_x_from_render = frame.xPos
-                curr_z_from_render = frame.zPos
-                print 'New position from render:',curr_x_from_render,',',curr_z_from_render,'after action:',self.actions[self.prev_a], #NSWE
-                if math.hypot( curr_x_from_render - expected_x, curr_z_from_render - expected_z ) > tol:
-                    print ' - ERROR DETECTED! Expected:',expected_x,',',expected_z
-                    raw_input("Press Enter to continue...")
-                else:
-                    print 'as expected.'
+            self.curr_x = obs[u'XPos']
+            self.curr_z = obs[u'ZPos']
+            print 'New position from observation:',self.curr_x,',',self.curr_z,
+            if math.hypot( self.curr_x - self.expected_x, self.curr_z - self.expected_z ) > self.tolerance:
+                print ' - ERROR DETECTED! Expected:',self.expected_x,',',self.expected_z
+                raw_input("Press Enter to continue...")
             else:
-                print
-            prev_x = curr_x
-            prev_z = curr_z
+                print 'as expected.'
+            curr_x_from_render = frame.xPos
+            curr_z_from_render = frame.zPos
+            print 'New position from render:',curr_x_from_render,',',curr_z_from_render,
+            if math.hypot( curr_x_from_render - self.expected_x, curr_z_from_render - self.expected_z ) > self.tolerance:
+                print ' - ERROR DETECTED! Expected:',self.expected_x,',',self.expected_z
+                raw_input("Press Enter to continue...")
+            else:
+                print 'as expected.'
+            self.prev_x = self.curr_x
+            self.prev_z = self.curr_z
             
         return world_state
         
     def act( self ):
-        pass # TODO
+        i_action = random.randint(0,len(self.actions)-1)
+        action = self.actions[ i_action ]
+        print 'Sending',action
+        self.agent_host.sendCommand( action )
+        if self.action_set == 'discrete_absolute':
+            self.expected_x = self.prev_x + [0,0,-1,1][i_action]
+            self.expected_z = self.prev_z + [-1,1,0,0][i_action]
+        
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 
+# -- set up the agent host --
 agent_host = MalmoPython.AgentHost()
-agent_host.addOptionalStringArgument('mission_file',
-    'Path/to/file from which to load the mission.', '../Sample_missions/cliff_walking_1.xml')
-
 try:
     agent_host.parse( sys.argv )
 except RuntimeError as e:
@@ -166,24 +166,44 @@ if agent_host.receivedArgument("help"):
     print agent_host.getUsage()
     exit(0)
 
-
-agent = RandomAgent( agent_host )
-
-# -- set up the mission -- #
-mission_file = agent_host.getStringArgument('mission_file')
-with open(mission_file, 'r') as f:
-    print "Loading mission from %s" % mission_file
-    mission_xml = f.read()
-    my_mission = MalmoPython.MissionSpec(mission_xml, True)
-
-my_clients = MalmoPython.ClientPool()
-my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
+# -- set up the mission --
+xml = '''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://ProjectMalmo.microsoft.com Mission.xsd">
+  <About>
+    <Summary/>
+  </About>
+  <ModSettings>
+    <MsPerTick>1</MsPerTick>
+  </ModSettings>
+  <ServerSection>
+    <ServerHandlers>
+      <FlatWorldGenerator forceReset="false" generatorString="3;7,220*1,5*3,2;3;,biome_1" seed=""/>
+      <ServerQuitFromTimeUp description="" timeLimitMs="130000"/>
+      <ServerQuitWhenAnyAgentFinishes description=""/>
+    </ServerHandlers>
+  </ServerSection>
+  <AgentSection mode="Survival">
+    <Name>Cristina</Name>
+    <AgentStart/>
+    <AgentHandlers>
+      <ObservationFromFullStats/>
+      <VideoProducer viewpoint="0" want_depth="false">
+        <Width>320</Width>
+        <Height>240</Height>
+      </VideoProducer>
+      <RewardForReachingPosition dimension="0">
+        <Marker oneshot="true" reward="100" tolerance="1.100000023841858" x="19.5" y="0" z="19.5"/>
+      </RewardForReachingPosition>
+      <RewardForSendingCommand reward="0" />
+      <DiscreteMovementCommands/>
+    </AgentHandlers>
+  </AgentSection>
+</Mission>'''
+my_mission = MalmoPython.MissionSpec(xml,True)
 
 max_retries = 3
-agentID = 0
-expID = 'tabular_q_learning'
 
-my_mission_record = MalmoPython.MissionRecordSpec( "./save_%s-map%d-rep%d.tgz" % (expID, imap, i) )
+my_mission_record = MalmoPython.MissionRecordSpec( 'robust_frames.tgz' )
 my_mission_record.recordCommands()
 my_mission_record.recordMP4(20, 400000)
 my_mission_record.recordRewards()
@@ -191,7 +211,7 @@ my_mission_record.recordObservations()
 
 for retry in range(max_retries):
     try:
-        agent_host.startMission( my_mission, my_clients, my_mission_record, agentID, "%s-%d" % (expID, i) )
+        agent_host.startMission( my_mission, my_mission_record )
         break
     except RuntimeError as e:
         if retry == max_retries - 1:
@@ -211,7 +231,9 @@ while not world_state.has_mission_begun:
 print
 
 # the main loop:
+action_set = 'discrete_absolute'
+agent = RandomAgent( agent_host, action_set )
 world_state = agent.waitForInitialState()
 while world_state.is_mission_running:
-    world_state = agent.waitForNextState()
     agent.act()
+    world_state = agent.waitForNextState()
