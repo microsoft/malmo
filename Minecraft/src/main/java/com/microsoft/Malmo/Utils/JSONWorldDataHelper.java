54d6ee8ae70511c26dbae0d0a2e306363be3d701
davey <a-dabign@microsoft.com>
2016-09-22 14:03:14 +0100

    Added world time info to full stats observation.

--- Minecraft/src/main/java/com/microsoft/Malmo/Utils/JSONWorldDataHelper.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/Utils/JSONWorldDataHelper.java
@@ -141,6 +141,11 @@ public class JSONWorldDataHelper
         json.addProperty("Yaw", player.rotationYaw);
     }
 
+    public static void buildEnvironmentStats(JsonObject json, EntityPlayerMP player)
+    {
+        json.addProperty("WorldTime", player.worldObj.getWorldTime());  // Ranges from 0 to 23999
+        json.addProperty("TotalTime", player.worldObj.getWorldTime());  // Total time world has been running
+    }
     /**
      * Build a signal for the cubic block grid centred on the player.<br>
      * Default is 3x3x4. (One cube all around the player.)<br>
