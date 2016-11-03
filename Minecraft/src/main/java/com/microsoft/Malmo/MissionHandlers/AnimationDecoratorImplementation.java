f3cfbfccf44f0c522cbfd166dedb0ab6b59b8cda
davey <a-dabign@microsoft.com>
2016-11-03 18:19:26 +0000

    Extended animation to allow control via parametric equations etc.

--- Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/AnimationDecoratorImplementation.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/AnimationDecoratorImplementation.java
@@ -20,6 +20,7 @@
 package com.microsoft.Malmo.MissionHandlers;
 
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.BlockPos;
@@ -28,8 +29,10 @@ import net.minecraft.world.World;
 
 import com.microsoft.Malmo.MissionHandlerInterfaces.IWorldDecorator;
 import com.microsoft.Malmo.Schemas.AnimationDecorator;
+import com.microsoft.Malmo.Schemas.AnimationDecorator.Linear;
 import com.microsoft.Malmo.Schemas.MissionInit;
 import com.microsoft.Malmo.Utils.AnimationDrawingHelper;
+import com.microsoft.Malmo.Utils.EvaluationHelper;
 
 /** WorldBuilder that takes a DrawingDecorator object and animates it.<br>
  */
@@ -41,6 +44,8 @@ public class AnimationDecoratorImplementation extends HandlerBase implements IWo
     Vec3 velocity;
     Vec3 minCanvas;
     Vec3 maxCanvas;
+    int tickCount;
+    Random rng;
 
     @Override
     public boolean parseParameters(Object params)
@@ -49,16 +54,50 @@ public class AnimationDecoratorImplementation extends HandlerBase implements IWo
             return false;
 
         this.params = (AnimationDecorator) params;
-        this.origin = new Vec3(this.params.getInitialPos().getX().doubleValue(), this.params.getInitialPos().getY().doubleValue(), this.params.getInitialPos().getZ().doubleValue());
-        this.velocity = new Vec3(this.params.getInitialVelocity().getX().doubleValue(), this.params.getInitialVelocity().getY().doubleValue(), this.params.getInitialVelocity().getZ().doubleValue());
-        this.minCanvas = new Vec3(this.params.getCanvasBounds().getMin().getX(), this.params.getCanvasBounds().getMin().getY(), this.params.getCanvasBounds().getMin().getZ());
-        this.maxCanvas = new Vec3(this.params.getCanvasBounds().getMax().getX(), this.params.getCanvasBounds().getMax().getY(), this.params.getCanvasBounds().getMax().getZ());
+
+        // Initialise starting positions / velocities:
+        if (this.params.getLinear() != null)
+        {
+            Linear linear = this.params.getLinear();
+            this.origin = new Vec3(linear.getInitialPos().getX().doubleValue(), linear.getInitialPos().getY().doubleValue(), linear.getInitialPos().getZ().doubleValue());
+            this.velocity = new Vec3(linear.getInitialVelocity().getX().doubleValue(), linear.getInitialVelocity().getY().doubleValue(), linear.getInitialVelocity().getZ().doubleValue());
+            this.minCanvas = new Vec3(linear.getCanvasBounds().getMin().getX(), linear.getCanvasBounds().getMin().getY(), linear.getCanvasBounds().getMin().getZ());
+            this.maxCanvas = new Vec3(linear.getCanvasBounds().getMax().getX(), linear.getCanvasBounds().getMax().getY(), linear.getCanvasBounds().getMax().getZ());
+        }
+        else
+        {
+            // Initialise a RNG:
+            long seed = 0;
+            String strSeed = this.params.getParametric().getSeed();
+            if (strSeed == null || strSeed == "" || strSeed.equals("random"))
+                seed = System.currentTimeMillis();
+            else
+                seed = Long.parseLong(strSeed);
+            this.rng = new Random(seed);
+            try
+            {
+                double x = EvaluationHelper.eval(this.params.getParametric().getX(), 0, this.rng);
+                double y = EvaluationHelper.eval(this.params.getParametric().getY(), 0, this.rng);
+                double z = EvaluationHelper.eval(this.params.getParametric().getZ(), 0, this.rng);
+                this.origin = new Vec3(x, y, z);
+            }
+            catch (Exception e)
+            {
+                // Malformed equations.
+                System.out.println("ERROR: malformed equations in animation - check these:");
+                System.out.println("        " + this.params.getParametric().getX());
+                System.out.println("        " + this.params.getParametric().getY());
+                System.out.println("        " + this.params.getParametric().getZ());
+            }
+        }
         return true;
     }
 
     @Override
-    public void buildOnWorld(MissionInit missionInit)
+    public void buildOnWorld(MissionInit missionInit) throws DecoratorException
     {
+        if (this.origin == null)
+            throw new DecoratorException("Origin not specified - check syntax of equations?");
         try
         {
             this.drawContext.setOrigin(this.origin);
@@ -66,43 +105,57 @@ public class AnimationDecoratorImplementation extends HandlerBase implements IWo
         }
         catch (Exception e)
         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            throw new DecoratorException("Error trying to build animation - " + e.getMessage());
         }
     }
 
     @Override
     public void update(World world)
     {
-        double dx = this.velocity.xCoord;
-        double dy = this.velocity.yCoord;
-        double dz = this.velocity.zCoord;
-        if (this.drawContext.getMax().xCoord + dx > this.params.getCanvasBounds().getMax().getX() + 1.0 || this.drawContext.getMin().xCoord + dx < this.params.getCanvasBounds().getMin().getX())
-            dx = -dx;
-        if (this.drawContext.getMax().yCoord + dy > this.params.getCanvasBounds().getMax().getY() + 1.0 || this.drawContext.getMin().yCoord + dy < this.params.getCanvasBounds().getMin().getY())
-            dy = -dy;
-        if (this.drawContext.getMax().zCoord + dz > this.params.getCanvasBounds().getMax().getZ() + 1.0 || this.drawContext.getMin().zCoord + dz < this.params.getCanvasBounds().getMin().getZ())
-            dz = -dz;
-        this.velocity = new Vec3(dx, dy, dz);
+        this.tickCount++;
         BlockPos oldpos = new BlockPos(this.origin);
-        this.origin = this.origin.add(this.velocity);
+        if (this.params.getLinear() != null)
+        {
+            Linear linear = this.params.getLinear();
+            double dx = this.velocity.xCoord;
+            double dy = this.velocity.yCoord;
+            double dz = this.velocity.zCoord;
+            if (this.drawContext.getMax().xCoord + dx > linear.getCanvasBounds().getMax().getX() + 1.0 || this.drawContext.getMin().xCoord + dx < linear.getCanvasBounds().getMin().getX())
+                dx = -dx;
+            if (this.drawContext.getMax().yCoord + dy > linear.getCanvasBounds().getMax().getY() + 1.0 || this.drawContext.getMin().yCoord + dy < linear.getCanvasBounds().getMin().getY())
+                dy = -dy;
+            if (this.drawContext.getMax().zCoord + dz > linear.getCanvasBounds().getMax().getZ() + 1.0 || this.drawContext.getMin().zCoord + dz < linear.getCanvasBounds().getMin().getZ())
+                dz = -dz;
+            this.velocity = new Vec3(dx, dy, dz);
+            this.origin = this.origin.add(this.velocity);
+        }
+        else
+        {
+            try
+            {
+                double x = EvaluationHelper.eval(this.params.getParametric().getX(), this.tickCount, this.rng);
+                double y = EvaluationHelper.eval(this.params.getParametric().getY(), this.tickCount, this.rng);
+                double z = EvaluationHelper.eval(this.params.getParametric().getZ(), this.tickCount, this.rng);
+                this.origin = new Vec3(x, y, z);
+            }
+            catch (Exception e)
+            {
+                // Just fail and move on.
+                System.out.println("ERROR - check syntax of equations for animation.");
+            }
+        }
         BlockPos newpos = new BlockPos(this.origin);
         if (oldpos.equals(newpos))
             return;
         try
         {
-            long t1 = System.currentTimeMillis();
             this.drawContext.setOrigin(this.origin);
             this.drawContext.Draw(this.params.getDrawingDecorator(), MinecraftServer.getServer().getEntityWorld());
-            long t2 = System.currentTimeMillis();
             this.drawContext.clearPrevious(MinecraftServer.getServer().getEntityWorld());
-            long t3 = System.currentTimeMillis();
-            System.out.println("Time: " + (t3-t1) + " (draw: " + (t2-t1) + ", clear: " + (t3-t2) + ")");
         }
         catch (Exception e)
         {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println("ERROR - can not draw animation.");
         }
     }
 
