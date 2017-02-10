176b72fbaaa6f3cf1b198e22661848072d20f21d
Katja Hofmann <kahofman@microsoft.com>
2017-02-10 10:52:55 +0000

    basic error check in install, fix cmake

--- scripts/malmo_install.ps1
+++ scripts/malmo_install.ps1
@@ -7,29 +7,39 @@ if (-Not (Test-Path .\temp))
     mkdir temp
 }
 
-# Install dependencies:
-Install-7Zip
-Install-Ffmpeg
-Install-Java
-Install-Python
-Install-XSD
-Install-VCRedist
-Install-Mesa
+$error.clear()
+try {
 
-# Now install Malmo:
-Display-Heading "Installing Minecraft"
+    # Install dependencies:
+    Install-7Zip
+    Install-Ffmpeg
+    Install-Java
+    Install-Python
+    Install-XSD
+    Install-VCRedist
+    Install-Mesa
 
-cd ..
-$MALMO_HOME = (Get-Item -Path ".\" -Verbose).FullName
-Add-MalmoXSDPathEnv $MALMO_HOME
+    # Now install Malmo:
+    Display-Heading "Installing Minecraft"
 
-Write-Output "Malmo installed successfully"
-Write-Output ""
-Write-Output "To start a Malmo client, use:"
-Write-Output ("cd " + $MALMO_HOME + "\Minecraft")
-Write-Output ".\launchClient.bat"
-Write-Output "To run a first Malmo example, open a separate PowerShell and run:"
-Write-Output ("cd " + $MALMO_HOME + "\Python_Examples")
-Write-Output ("python tabular_q_learning.py")
-Write-Output ""
-Write-Output ("For more details see the tutorial in " + $MALMO_HOME + "\Documentation\index.html")
+    cd ..
+    $MALMO_HOME = (Get-Item -Path ".\" -Verbose).FullName
+    Add-MalmoXSDPathEnv $MALMO_HOME
+
+} catch {
+    Write-Output "An error occurred, please check for error messages above. If the error persists, please raise an issue on https://github.com/Microsoft/malmo."
+}
+
+if (!$error) {
+
+    Write-Output "Malmo installed successfully"
+    Write-Output ""
+    Write-Output "To start a Malmo client, use:"
+    Write-Output ("cd " + $MALMO_HOME + "\Minecraft")
+    Write-Output ".\launchClient.bat"
+    Write-Output "To run a first Malmo example, open a separate PowerShell and run:"
+    Write-Output ("cd " + $MALMO_HOME + "\Python_Examples")
+    Write-Output ("python tabular_q_learning.py")
+    Write-Output ""
+    Write-Output ("For more details see the tutorial in " + $MALMO_HOME + "\Documentation\index.html")
+}
