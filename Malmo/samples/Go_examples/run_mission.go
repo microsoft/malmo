4a34920d40df66bf6723f5bb98042251ddc225f3
Dorival Pedroso <pedroso@cpmech.com>
2017-04-09 23:01:15 +1000

    Add copyright message

--- Malmo/samples/Go_examples/run_mission.go
+++ Malmo/samples/Go_examples/run_mission.go
@@ -17,8 +17,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 // --------------------------------------------------------------------------------------------------
 
-// +build ignore
-
 package main
 
 import (
