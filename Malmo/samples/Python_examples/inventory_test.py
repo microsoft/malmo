4d091e9daecd1b67ec0f08e2ab28a2a5d11d2a26
DaveyBiggers <DaveyBignell@gmail.com>
2017-07-17 22:35:22 +0100

    Finished inventory test sample

--- Malmo/samples/Python_examples/inventory_test.py
+++ Malmo/samples/Python_examples/inventory_test.py
@@ -17,6 +17,22 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ------------------------------------------------------------------------------------------------
 
+# Test of Malmo's extended inventory feature:
+# - DrawingDecorator allows you to "draw" containers with specific contents
+# - ObservationFromFullInventory includes contents of the container under focus
+# - InventoryCommands allows you to transfer items between the player's inventory and the container
+
+# To test, create sixteen shulker boxes - one for each colour - arranged in a circle.
+# Take four stained glass tiles of each colour - 64 tiles in total - and shuffle them, putting four in each box.
+# Stand the agent in the middle, rotating so that it "sees" each container in turn.
+# "Sort" the tiles by collecting all the wrong coloured tiles from the box,
+# and placing all the right coloured tiles into the box.
+
+# (In theory, after two complete rotations the boxes should all be sorted.
+# In practice, it may take longer, since each inventory swap command takes a finite time to process,
+# and the agent keeps rotating at a fixed speed, so may not have time to complete all the swaps before
+# moving on to the next box.)
+
 import MalmoPython
 import json
 import math
@@ -26,8 +42,53 @@ import sys
 import time
 from collections import namedtuple
 
+# Create a named tuple type for the inventory contents.
 InventoryObject = namedtuple('InventoryObject', 'type, colour, variation, quantity, inventory, index')
 InventoryObject.__new__.__defaults__ = ("", "", "", 0, "", 0)
+
+# The sixteen Minecraft colours - see Types.xsd
+colours = ["WHITE", "ORANGE", "MAGENTA", "LIGHT_BLUE", "YELLOW", "LIME", "PINK", "GRAY", "SILVER", "CYAN", "PURPLE", "BLUE", "BROWN", "GREEN", "RED", "BLACK"]
+
+def boxCompleted(inventoryFull, colour):
+    '''Check whether the box included in this inventory is complete.'''
+    inventory = [i for i in inventoryFull if i.inventory != "inventory"]    # Filter out the box's items from the player's.
+    # A box is considered completed if it contains exactly four items of the correct colour,
+    # occupying the first four slots.
+    if len(inventory) != 4:
+        return False    # Wrong number of items in inventory
+    slotFilled = [False for x in xrange(4)]
+    for i in inventory:
+        if i.colour != colour:
+            return False    # Item of wrong colour in inventory
+        if i.index >= 4 or i.index < 0:
+            return False    # Item in wrong slot
+        slotFilled[i.index] = True
+    if False in slotFilled:
+        return False    # Not all of the first four slots are filled
+    return True # All good
+
+def getContainerXML():
+    '''Place 16 shulker boxes in Minecraft's equivalent of a circle, and fill each one with four randomly chosen coloured tiles.'''
+    xml = ""
+    radius = 4.0
+    # A greater radius gives a more "circular" circle, but need to keep the boxes within range of the agent.
+    contents = 4 * ['<Object type="stained_glass" colour="{}" quantity="1"/>'.format(col) for col in colours] # Four tiles for each colour
+    for i, col in enumerate(colours):
+        ang = math.pi * float(i) / 8.0
+        x = int(radius * math.cos(ang))
+        z = int(radius * math.sin(ang))
+        type = col.lower() + "_shulker_box"
+        xml += '<DrawContainer x="{x}" y="56" z="{z}" type="{type}">'.format(x = x, z = z, type = type)
+        # Fill with four random entries from the contents list.
+        # To ensure we get no accidental duplicates, chose each item from the
+        # items remaining, and swap it out after use.
+        # (eg https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm)
+        for content_index in xrange(i * 4, (i + 1) * 4):
+            rand = random.randint(content_index, len(contents) - 1)
+            xml += contents[rand]
+            contents[rand], contents[content_index] = contents[content_index], contents[rand]
+        xml += '</DrawContainer>'
+    return xml
 
 missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
     <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -37,36 +98,8 @@ missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
 
         <ServerSection>
             <ServerHandlers>
-                <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;157;decoration,lake" />
-                <DrawingDecorator>
-                    <DrawContainer x="0" y="56" z="422" type="chest">
-                        <object type="boat" quantity="4"/>
-                        <object type="paper" quantity="1"/>
-                        <object type="bucket" quantity="100"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="423" type="dispenser">
-                        <object type="apple" quantity="100"/>
-                        <object type="cake" quantity="100"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="424" type="dropper">
-                        <object type="skull" quantity="100"/>
-                        <object type="map" quantity="100"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="425" type="ender_chest">
-                        <object type="rabbit" quantity="100"/>
-                        <object type="carrot_on_a_stick" quantity="100"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="426" type="hopper">
-                        <object type="quartz" quantity="10"/>
-                        <object type="diamond" quantity="10"/>
-                        <object type="prismarine" quantity="10"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="427" type="red_shulker_box">
-                        <object type="black_shulker_box" quantity="100"/>
-                    </DrawContainer>
-                    <DrawContainer x="0" y="56" z="428" type="ender_chest">
-                        <object type="black_shulker_box" quantity="100"/>
-                    </DrawContainer>
+                <FlatWorldGenerator generatorString="3;7,44*49,73,35:1,159:4,95:13,35:13,159:11,95:10,159:14,159:6,35:6,95:6;157;" />
+                <DrawingDecorator>''' + getContainerXML() + '''
                 </DrawingDecorator>
                 <ServerQuitFromTimeUp timeLimitMs="60000" description="out_of_time"/>
                 <ServerQuitWhenAnyAgentFinishes />
@@ -76,10 +109,15 @@ missionXML = '''<?xml version="1.0" encoding="UTF-8" ?>
         <AgentSection mode="Survival">
             <Name>Lovejoy</Name>
             <AgentStart>
-                <Placement x="0.5" y="56.0" z="420.5"/>
+                <Placement x="0.5" y="56.0" z="0.5" pitch="18"/>
+                <Inventory/>
             </AgentStart>
             <AgentHandlers>
                 <ObservationFromFullInventory flat="false"/>
+                <ObservationFromRay/>
+                <InventoryCommands/>
+                <ContinuousMovementCommands />
+                <MissionQuitCommands />
             </AgentHandlers>
         </AgentSection>
 
@@ -105,6 +143,7 @@ for retry in range(max_retries):
         agent_host.startMission( my_mission, my_mission_record )
         break
     except RuntimeError as e:
+        print e
         if retry == max_retries - 1:
             print "Error starting mission",e
             print "Is the game running?"
@@ -116,15 +155,111 @@ world_state = agent_host.peekWorldState()
 while not world_state.has_mission_begun:
     time.sleep(0.1)
     world_state = agent_host.peekWorldState()
-    
+
+# Start the agent turning:
+agent_host.sendCommand("turn 0.2")
+
+last_inventory = None
+last_box_colour = None
+boxes_traversed = 0
+num_swaps = 0
+# Track which boxes have been sorted:
+completed_boxes = { col:False for col in colours }
+
 while world_state.is_mission_running:
-    world_state = agent_host.peekWorldState()
+    world_state = agent_host.getWorldState()
     if world_state.number_of_observations_since_last_state > 0:
         obs = json.loads(world_state.observations[-1].text)
-        inv = [InventoryObject(**k) for k in obs[u'inventory']]
-        for i in inv:
-            print i.quantity, " x ", i.type, i.inventory
-        #print obs
+
+        # Are we looking at a shulker box? If so, we want to know its colour.
+        # We use the ObservationFromRay to find this out.
+        box_colour = None
+        if u'LineOfSight' in obs:
+            los = obs[u'LineOfSight']
+            box_colour = los[u'type'].split("_shulker")[0].upper() # eg "brown_shulker_box" -> "BROWN"
+            if not box_colour in colours:
+                continue
+            if box_colour != last_box_colour:
+                last_box_colour = box_colour
+                boxes_traversed += 1
+
+        # In our setup, we know that the "foreign" inventories will all be named "shulkerBox",
+        # but here is how we could check for the presence of an inventory by using the "inventoriesAvailable"
+        # data that ObservationFromFullInventory returns. We also use this to get the size of the
+        # inventories.
+        # (Note that we can also get the inventory names from the list of inventory contents - but if there
+        # is nothing in the inventory this method will fail.)
+        invSizeLocal, invSizeForeign = 0, 0
+        foreignInventory = ""
+        if u'inventoriesAvailable' in obs:
+            for inv in obs[u'inventoriesAvailable']:
+                if inv[u'name'] == "inventory": # Player's inventory is always called "inventory".
+                    invSizeLocal = inv[u'size']
+                else:
+                    foreignInventory = str(inv[u'name'])
+                    invSizeForeign = inv[u'size']
+
+        # Now look for potential swaps. We need this check against last_inventory because
+        # the swap command, and the observation generation, take place on the Minecraft server. This adds
+        # some lag, meaning that we won't instantly see the result of the swap - and so the code which looks
+        # for useful swaps will keep generating the same swap command (thus undoing itself).
+        if last_inventory != obs[u'inventory']:
+            # The inventory has changed, so proceed.
+            last_inventory = obs[u'inventory']           
+
+            # Create a list of the available contents.
+            # This will create an InventoryObject for every Minecraft ItemStack in both the player's
+            # inventory, and the "foreign" inventory (our shulker box).
+            inv = [InventoryObject(**k) for k in obs[u'inventory']]
+            if boxCompleted(inv, box_colour):
+                if not completed_boxes[box_colour]:
+                    completed_boxes[box_colour] = True
+                    print "Completed " + box_colour + " box."
+                    if not False in completed_boxes.values():
+                        print "ALL BOXES COMPLETED!"
+                        agent_host.sendCommand("quit")
+                        time.sleep(1)
+
+            # We want to track empty slots in both inventories. To do this, we create a list with a
+            # numbered entry for each slot, and whenever we encounter an item we mark its slot as used
+            # by setting that entry to a (high) sentinel value. We can then find the lowest available
+            # slot by finding the min value in the list.
+            ourUsedSlots = range(invSizeLocal)
+            theirUsedSlots = range(invSizeForeign)
+
+            # Now look for a good swap.
+            # The best-case scenario is swapping a wrong coloured item for a right coloured item.
+            # If we have no right-coloured items to swap in, then swap the wrong coloured items with an
+            # empty slot in our inventory.
+            # If the box has no wrong-coloured item to swap out, then swap with an empty slot in the box's inventory.
+            # If there is nothing to swap in, and nothing to swap out... do nothing.
+            wrongColouredItem, rightColouredItem = None, None
+            for i in inv:
+                if i.inventory == "inventory":
+                    ourUsedSlots[i.index] = invSizeLocal
+                    if i.colour == box_colour and rightColouredItem == None:
+                        rightColouredItem = i
+                else:
+                    theirUsedSlots[i.index] = invSizeForeign
+                    if i.colour != box_colour and wrongColouredItem == None:
+                        wrongColouredItem = i
+
+            ourFirstSlot = min(ourUsedSlots)
+            theirFirstSlot = min(theirUsedSlots)
+            source = str(rightColouredItem.index) if rightColouredItem else str(ourFirstSlot)
+            dest = str(foreignInventory) + ":" + (str(wrongColouredItem.index) if wrongColouredItem else str(theirFirstSlot))
+            sourceCol = rightColouredItem.colour if rightColouredItem else "empty"
+            destCol = wrongColouredItem.colour if wrongColouredItem else "empty"
+            if rightColouredItem or wrongColouredItem:
+                print "Swapping " + source + "(" + sourceCol + ") and " + dest + "(" + destCol + ")"
+                num_swaps += 1
+                agent_host.sendCommand("swapInventoryItems " + source + " " + dest)
 
 # mission has ended.
-print "Mission over - feel free to explore the world."
+if False in completed_boxes.values():
+    print "FAILED TO SORT BOXES: "
+    print [k for k in completed_boxes if not completed_boxes[k]]
+    if agent_host.receivedArgument("test"):
+        exit(1)
+else:
+    print "Mission over - sorted boxes with " + str(num_swaps) + " swap commands (" + str(float(boxes_traversed)/16.0) + " laps)."
