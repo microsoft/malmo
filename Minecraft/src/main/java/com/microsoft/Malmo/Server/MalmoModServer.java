ecffef9ce58ff025da8d7972cf99b7a53d0086f4
DaveyBiggers <DaveyBignell@gmail.com>
2018-01-12 12:32:25 +0000

    Added diagnostic observations

--- Minecraft/src/main/java/com/microsoft/Malmo/Server/MalmoModServer.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/Server/MalmoModServer.java
@@ -21,40 +21,63 @@ package com.microsoft.Malmo.Server;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.relauncher.Side;
 
 import com.microsoft.Malmo.Schemas.MissionInit;
+import com.microsoft.Malmo.Utils.TimeHelper;
 
 public class MalmoModServer
 {
-	private ServerStateMachine stateMachine;
-	
-	/** Called when creating a dedicated server
-	 */
-	public void init(FMLInitializationEvent event)
-	{
-		initBusses();
+    private ServerStateMachine stateMachine;
+    private TimeHelper.TickRateMonitor serverTickMonitor = new TimeHelper.TickRateMonitor();
+
+    /**
+     * Called when creating a dedicated server
+     */
+    public void init(FMLInitializationEvent event)
+    {
+        initBusses();
         this.stateMachine = new ServerStateMachine(ServerState.WAITING_FOR_MOD_READY);
-	}
+    }
 
-	/** Called when creating an integrated server
-	 */
-	public void init(MissionInit minit)
-	{
-		initBusses();
+    /**
+     * Called when creating an integrated server
+     */
+    public void init(MissionInit minit)
+    {
+        initBusses();
         this.stateMachine = new ServerStateMachine(ServerState.WAITING_FOR_MOD_READY, minit);
-	}
-	
-	/** Provides a direct way for the owner of the integrated server to send a new mission init message.<br>
-	 * Don't call this unless the server has been initialised first.*/
-	public void sendMissionInitDirectToServer(MissionInit minit)
-	{
-		this.stateMachine.setMissionInit(minit);
-	}
+    }
 
-	private void initBusses()
-	{
+    /**
+     * Provides a direct way for the owner of the integrated server to send a new mission init message.<br>
+     * Don't call this unless the server has been initialised first.
+     */
+    public void sendMissionInitDirectToServer(MissionInit minit)
+    {
+        this.stateMachine.setMissionInit(minit);
+    }
+
+    private void initBusses()
+    {
         // Register for various events:
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    @SubscribeEvent
+    public void onServerTick(TickEvent.ServerTickEvent ev)
+    {
+        if (ev.side == Side.SERVER && ev.phase == Phase.START)
+        {
+            this.serverTickMonitor.beat();
+        }
+    }
+
+    public float getServerTickRate()
+    {
+        return this.serverTickMonitor.getEventsPerSecond();
+    }
 }
