5b23862e7ee8fb5a3769ac3a8b10d2220ab2916e
DaveyBiggers <DaveyBignell@gmail.com>
2018-03-16 14:57:07 +0000

    Missing print function for py2

--- Malmo/samples/Python_examples/braitenberg_simulation.py
+++ Malmo/samples/Python_examples/braitenberg_simulation.py
@@ -17,6 +17,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # ------------------------------------------------------------------------------------------------
 
+from __future__ import print_function
 import MalmoPython
 import os
 import sys
