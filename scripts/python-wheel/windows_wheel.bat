c3987d1772db911ae62aad5f0bab16bb3d9e19eb
AndKram <v-andkra@microsoft.com>
2018-06-04 11:57:24 +0100

    fix wheel scripts cp commands

--- scripts/python-wheel/windows_wheel.bat
+++ scripts/python-wheel/windows_wheel.bat
@@ -1,10 +1,9 @@
 pip3 install twine wheel
-copy ..\..\LICENSE.txt package
 copy ..\..\build\install\Python_Examples\MalmoPython.lib package\malmo
 copy ..\..\build\install\Python_Examples\MalmoPython.pyd package\malmo
 copy ..\..\build\install\Python_Examples\malmoutils.py package\malmo
 copy ..\..\build\install\Python_Examples\run_mission.py package\malmo
-copy ..\..\Minecraft\launch_minecraft_in_background.py
+copy ..\..\Minecraft\launch_minecraft_in_background.py paclage\malmo
 
 cd package
 python setup.py bdist_wheel
