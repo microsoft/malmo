a0e60dba9bba65a11118ebf3fb619e27ad649fef
AndKram <v-andkra@microsoft.com>
2018-08-10 14:45:20 +0100

    parse score log

--- Minecraft/src/main/java/com/microsoft/Malmo/Utils/ScoreHelper.java
+++ Minecraft/src/main/java/com/microsoft/Malmo/Utils/ScoreHelper.java
@@ -68,7 +68,7 @@ public class ScoreHelper
             setLogging(Level.INFO);
         }
         if (logging)
-            log("Score policy is " + scoringPolicy);
+            log("<ScoreInit><Policy>" + scoringPolicy + "</Policy></ScoreInit>");
     }
 
     public static boolean isScoring() { return scoringPolicy > 0; }
@@ -90,12 +90,12 @@ public class ScoreHelper
         List<AgentSection> agents = missionInit.getMission().getAgentSection();
         String agentName = agents.get(missionInit.getClientRole()).getName();
 
-        StringBuffer message = new StringBuffer("<MissionInitDigest>");
+        StringBuffer message = new StringBuffer("<MissionInit><MissionDigest>");
         message.append(hash);
-        message.append("</MissionInitDigest>");
+        message.append("</MissionDigest>");
         message.append("<AgentName>");
         message.append(agentName);
-        message.append("</AgentName>");
+        message.append("</AgentName></MissionInit>");
         if (scoringPolicy == TOTAL_WITH_MISSION_XML)
             message.append(missionXml);
         log(message.toString());
