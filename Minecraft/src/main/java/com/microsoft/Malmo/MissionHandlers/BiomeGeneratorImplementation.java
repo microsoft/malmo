a8ccba55207817d73dd1b79aa44c6e41fecc03b2
AndKram <v-andkra@microsoft.com>
2018-10-12 12:48:01 +0100

    build & run exisiting tests

---
+++ Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/BiomeGeneratorImplementation.java
@@ -0,0 +1,144 @@
+// --------------------------------------------------------------------------------------------------
+//  Copyright (c) 2016 Microsoft Corporation
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+//  associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in all copies or
+//  substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+//  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// --------------------------------------------------------------------------------------------------
+
+package com.microsoft.Malmo.MissionHandlers;
+
+import java.util.Random;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.GameType;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.gen.layer.GenLayer;
+import net.minecraft.world.gen.layer.IntCache;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.terraingen.WorldTypeEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import com.microsoft.Malmo.MissionHandlerInterfaces.IWorldGenerator;
+import com.microsoft.Malmo.Schemas.BiomeGenerator;
+import com.microsoft.Malmo.Schemas.MissionInit;
+import com.microsoft.Malmo.Utils.MapFileHelper;
+
+/**
+ * Generates a survival world of only the biome specified.
+ * 
+ * @author Cayden Codel, Carnegie Mellon University
+ *
+ */
+public class BiomeGeneratorImplementation extends HandlerBase implements IWorldGenerator {
+
+	BiomeGenerator bparams;
+
+	// Register the event with the Forge Bus
+	public BiomeGeneratorImplementation() {
+		MinecraftForge.TERRAIN_GEN_BUS.register(this);
+	}
+
+	/**
+	 * Keeps a constant biome given an id number
+	 * 
+	 * @author Cayden Codel, Carnegie Mellon University Some contributions from
+	 *         teamrtg with the LonelyBiome mod
+	 *
+	 */
+	private class GenLayerConstant extends GenLayer {
+		private final int value;
+
+		public GenLayerConstant(int value) {
+			super(0L);
+			this.value = value;
+		}
+
+		@Override
+		public int[] getInts(int par1, int par2, int par3, int par4) {
+			int[] aint2 = IntCache.getIntCache(par3 * par4);
+
+			for (int i = 0; i < aint2.length; i++) {
+				aint2[i] = value;
+			}
+
+			return aint2;
+		}
+	}
+
+	// Make sure that the biome is one type with an event on biome gen
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void onBiomeGenInit(WorldTypeEvent.InitBiomeGens event) {
+		// Negative one is flag value for normal world gen
+		if (bparams.getBiome() == -1)
+			return;
+		GenLayer[] replacement = new GenLayer[2];
+		replacement[0] = new GenLayerConstant(bparams.getBiome());
+		replacement[1] = replacement[0];
+		event.setNewBiomeGens(replacement);
+	}
+
+	@Override
+	public boolean parseParameters(Object params) {
+		if (params == null || !(params instanceof BiomeGenerator))
+			return false;
+
+		this.bparams = (BiomeGenerator) params;
+		return true;
+	}
+
+	public static long getWorldSeedFromString() {
+		// This seed logic mirrors the Minecraft code in
+		// GuiCreateWorld.actionPerformed:
+		long seed = (new Random()).nextLong();
+		return seed;
+	}
+
+	@Override
+	public boolean createWorld(MissionInit missionInit) {
+		long seed = getWorldSeedFromString();
+		WorldType.WORLD_TYPES[0].onGUICreateWorldPress();
+		WorldSettings worldsettings = new WorldSettings(seed, GameType.SURVIVAL, true, false, WorldType.WORLD_TYPES[0]);
+		worldsettings.enableCommands();
+		// Create a filename for this map - we use the time stamp to make sure
+		// it is different from other worlds, otherwise no new world
+		// will be created, it will simply load the old one.
+		return MapFileHelper.createAndLaunchWorld(worldsettings, this.bparams.isDestroyAfterUse());
+	}
+
+	@Override
+	public boolean shouldCreateWorld(MissionInit missionInit, World world) {
+		if (this.bparams != null && this.bparams.isForceReset())
+			return true;
+
+		if (Minecraft.getMinecraft().world == null || world == null)
+			return true; // Definitely need to create a world if there isn't one
+		                 // in existence!
+
+		String genOptions = world.getWorldInfo().getGeneratorOptions();
+		if (genOptions != null && !genOptions.isEmpty())
+			return true; // Biome generator has no generator options.
+
+		return false;
+	}
+
+	@Override
+	public String getErrorDetails() {
+		return ""; // Don't currently have any error exit points.
+	}
+
+}
---
+++ Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/NavigationDecoratorImplementation.java
@@ -0,0 +1,162 @@
+// --------------------------------------------------------------------------------------------------
+//  Copyright (c) 2016 Microsoft Corporation
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+//  associated documentation files (the "Software"), to deal in the Software without restriction,
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute,
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or
+//  substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+//  NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// --------------------------------------------------------------------------------------------------
+package com.microsoft.Malmo.MissionHandlers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.microsoft.Malmo.MissionHandlerInterfaces.IWorldDecorator;
+import com.microsoft.Malmo.Schemas.MissionInit;
+import com.microsoft.Malmo.Schemas.NavigationDecorator;
+import com.microsoft.Malmo.Utils.MinecraftTypeHelper;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Creates a decorator that sets a random block and then points all compasses
+ * towards the block.
+ * 
+ * @author Cayden Codel, Carnegie Mellon University
+ *
+ */
+public class NavigationDecoratorImplementation extends HandlerBase implements IWorldDecorator {
+
+	private NavigationDecorator nparams;
+
+	private double originX, originY, originZ;
+	private double placementX, placementY, placementZ;
+	private double radius;
+	private double minDist, maxDist;
+
+	@Override
+	public boolean parseParameters(Object params) {
+		if (params == null || !(params instanceof NavigationDecorator))
+			return false;
+		this.nparams = (NavigationDecorator) params;
+		return true;
+	}
+
+	@Override
+	public void buildOnWorld(MissionInit missionInit, World world) throws DecoratorException {
+		if (nparams.getRandomPlacementProperties().getOrigin() != null)
+			originX = nparams.getRandomPlacementProperties().getOrigin().getX().doubleValue();
+		else
+			originX = world.getSpawnPoint().getX();
+
+		if (nparams.getRandomPlacementProperties().getOrigin() != null)
+			originY = nparams.getRandomPlacementProperties().getOrigin().getY().doubleValue();
+		else
+			originY = world.getSpawnPoint().getY();
+
+		if (nparams.getRandomPlacementProperties().getOrigin() != null)
+			originZ = nparams.getRandomPlacementProperties().getOrigin().getZ().doubleValue();
+		else
+			originZ = world.getSpawnPoint().getZ();
+
+		radius = nparams.getRandomPlacementProperties().getRadius().doubleValue();
+		minDist = nparams.getMinRandomizedDistance().doubleValue();
+		maxDist = nparams.getMaxRandomizedDistance().doubleValue();
+
+		placementX = 0;
+		placementY = 0;
+		placementZ = 0;
+
+		if (nparams.getRandomPlacementProperties().getPlacement().equals("surface")) {
+			placementX = ((Math.random() - 0.5) * 2 * radius);
+			placementZ = (Math.random() > 0.5 ? -1 : 1) * Math.sqrt((radius * radius) - (placementX * placementX));
+
+			// Change center to origin now
+			placementX += originX;
+			placementZ += originZ;
+			placementY = world.getTopSolidOrLiquidBlock(new BlockPos(placementX, 0, placementZ)).getY() - 1;
+		} else if (nparams.getRandomPlacementProperties().getPlacement().equals("circle")) {
+			placementX = ((Math.random() - 0.5) * 2 * radius);
+			placementY = originY;
+			placementZ = (Math.random() > 0.5 ? -1 : 1) * Math.sqrt((radius * radius) - (placementX * placementX));
+
+			// Change center to origin now
+			placementX += originX;
+			placementZ += originZ;
+		} else {
+			placementX = ((Math.random() - 0.5) * 2 * radius);
+			placementY = (Math.random() - 0.5) * 2 * Math.sqrt((radius * radius) - (placementX * placementX));
+			placementZ = (Math.random() > 0.5 ? -1 : 1)
+					* Math.sqrt((radius * radius) - (placementX * placementX) - (placementY * placementY));
+
+			// Change center to origin now
+			placementX += originX;
+			placementY += originY;
+			placementZ += originZ;
+		}
+
+		IBlockState state = MinecraftTypeHelper
+				.ParseBlockType(nparams.getRandomPlacementProperties().getBlock().value());
+		world.setBlockState(new BlockPos(placementX, placementY, placementZ), state);
+
+		// Set compass location to the block
+		double xDel = 0, zDel = 0;
+		if (nparams.isRandomizeCompassLocation()) {
+			double dist = 0;
+			do {
+				xDel = (Math.random() - 0.5) * 2 * maxDist;
+				zDel = (Math.random() - 0.5) * 2 * maxDist;
+				dist = Math.sqrt(xDel * xDel + zDel * zDel);
+			} while (dist <= maxDist && dist >= minDist);
+		}
+
+		placementX += xDel;
+		placementZ += zDel;
+	}
+
+	@Override
+	public boolean getExtraAgentHandlersAndData(List<Object> handlers, Map<String, String> data) {
+		return false;
+	}
+
+	@Override
+	public void update(World world) {
+		if (Minecraft.getMinecraft().player != null) {
+			BlockPos spawn = Minecraft.getMinecraft().player.world.getSpawnPoint();
+			if (spawn.getX() != (int) placementX && spawn.getY() != (int) placementY
+					&& spawn.getZ() != (int) placementZ)
+				Minecraft.getMinecraft().player.world.setSpawnPoint(new BlockPos(placementX, placementY, placementZ));
+		}
+	}
+
+	@Override
+	public void prepare(MissionInit missionInit) {
+	}
+
+	@Override
+	public void cleanup() {
+	}
+
+	@Override
+	public boolean targetedUpdate(String nextAgentName) {
+		return false;
+	}
+
+	@Override
+	public void getTurnParticipants(ArrayList<String> participants, ArrayList<Integer> participantSlots) {
+	}
+}
