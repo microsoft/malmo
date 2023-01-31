b10b14b2d3fbef37d2cf739737e38a915ff76533
Cassidy Laidlaw <cassidy256@gmail.com>
2023-01-30 19:21:42 -0800

    Forgot to add install_boost script

---
+++ scripts/python-wheel/package/install_boost.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+tar xf /boost/boost_1_76_0.tar.bz2
+cd boost_1_76_0
+./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,iostreams,program_options,python,regex,system,thread
+./b2 install
