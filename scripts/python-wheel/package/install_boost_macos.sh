ab55a2a47dbc04a9a5a0bf7c97521d5496bbfb8f
Cassidy Laidlaw <cassidy256@gmail.com>
2023-01-31 18:36:05 -0800

    Try cp39 on local runner

--- scripts/python-wheel/package/install_boost_macos.sh
+++ scripts/python-wheel/package/install_boost_macos.sh
@@ -5,12 +5,16 @@ set -x
 cd boost_1_76_0
 ./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,iostreams,program_options,python,regex,system,thread
 
-if [[ "$(file $(which python))" == *x86_64* ]]; then
+PY_PROCESSOR="$(python -c 'import platform; print(platform.processor())')"
+if [[ "$PY_PROCESSOR" == *86 ]]; then
     B2_ARCH=x86
     CXX_ARCH=x86_64
-else
+elif [[ "$PY_PROCESSOR" == arm ]]; then
     B2_ARCH=arm
     CXX_ARCH=arm64
+else
+    echo Unknown processor: $PY_PROCESSOR
+    exit 1
 fi
 
 PY_INCLUDE_PATH="$(python -c 'from sysconfig import get_paths; print(get_paths()["include"])')"
