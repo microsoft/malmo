17b9670bc3b4f70f883c884c98c657d921e5b170
Cassidy Laidlaw <cassidy256@gmail.com>
2023-01-31 16:47:11 -0800

    Fix issues with building wheels on macOS

--- scripts/python-wheel/package/install_boost_macos.sh
+++ scripts/python-wheel/package/install_boost_macos.sh
@@ -4,4 +4,8 @@ set -x
 
 cd boost_1_76_0
 ./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,iostreams,program_options,python,regex,system,thread
-./b2 install --prefix=. architecture=combined cxxflags="-arch x86_64 -arch arm64"
+if [[ "$(file $(which python))" == *x86_64* ]]; then
+    ./b2 architecture=x86 cxxflags="-arch x86_64" install --prefix=.
+else
+    ./b2 architecture=arm cxxflags="-arch arm64" install --prefix=.
+fi
