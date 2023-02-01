062f3bc0aff06eb0e32858db68b895bd2e4266aa
Cassidy Laidlaw <cassidy256@gmail.com>
2023-01-31 17:14:41 -0800

    Fix Boost not finding python headers in macOS

--- scripts/python-wheel/package/install_boost_macos.sh
+++ scripts/python-wheel/package/install_boost_macos.sh
@@ -4,8 +4,14 @@ set -x
 
 cd boost_1_76_0
 ./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,iostreams,program_options,python,regex,system,thread
+
 if [[ "$(file $(which python))" == *x86_64* ]]; then
-    ./b2 architecture=x86 cxxflags="-arch x86_64" install --prefix=.
+    B2_ARCH=x86
+    CXX_ARCH=x86_64
 else
-    ./b2 architecture=arm cxxflags="-arch arm64" install --prefix=.
+    B2_ARCH=arm
+    CXX_ARCH=arm64
 fi
+
+PY_INCLUDE_PATH="$(python -c 'from sysconfig import get_paths; print(get_paths()["include"])')"
+./b2 architecture=$B2_ARCH cxxflags="-arch $CXX_ARCH -I${PY_INCLUDE_PATH}" install --prefix=. -d0
