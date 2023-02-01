c955238e2a35f56a0f7e0ba79b15763452b43b95
Cassidy Laidlaw <cassidy256@gmail.com>
2023-01-31 17:32:35 -0800

    Cache downloads in wheel building

--- scripts/python-wheel/package/install_boost_linux.sh
+++ scripts/python-wheel/package/install_boost_linux.sh
@@ -2,7 +2,6 @@
 
 set -x
 
-tar xf /boost/boost_1_76_0.tar.bz2
 cd boost_1_76_0
 ./bootstrap.sh --with-libraries=atomic,chrono,date_time,filesystem,iostreams,program_options,python,regex,system,thread
 ./b2 install -d0
