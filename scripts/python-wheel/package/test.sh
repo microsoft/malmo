c54fdb236067ec8f5deed0a429213253f8526b98
Cassidy Laidlaw <cassidy256@gmail.com>
2023-02-02 08:22:52 -0800

    Avoid endless loops in test.sh

--- scripts/python-wheel/package/test.sh
+++ scripts/python-wheel/package/test.sh
@@ -7,7 +7,10 @@ set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python -m malmo.minecraft launch --timeout 360 &
-until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 5; jobs; done
+MC_PID=$!
+until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ] || ! kill -0 "$MC_PID"; do
+    sleep 5; jobs
+done
 python "${SCRIPT_DIR}/tests/run_mission.py" -v
 
 kill $(jobs -p)
