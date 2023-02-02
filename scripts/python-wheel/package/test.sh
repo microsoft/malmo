564e276228fb2f69ea5518fc925792d957716e21
Cassidy Laidlaw <cassidy256@gmail.com>
2023-02-01 16:31:19 -0800

    Configure testing in cibuildwheel

---
+++ scripts/python-wheel/package/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+set -x
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    DISPLAY=:1
+    Xvfb $DISPLAY &
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+python -m malmo.minecraft launch &
+until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 1; done
+python "${SCRIPT_DIR}/tests/run_mission.py"
