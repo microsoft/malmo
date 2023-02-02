30fc1fe6c37c5c19388f9be4acd8bff54ce4116f
Cassidy Laidlaw <cassidy256@gmail.com>
2023-02-02 08:05:13 -0800

    Try building both python 3.8 and 3.9 wheels

--- scripts/python-wheel/package/test.sh
+++ scripts/python-wheel/package/test.sh
@@ -7,7 +7,7 @@ set -x
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python -m malmo.minecraft launch --timeout 360 &
-until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 1; jobs; done
+until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 5; jobs; done
 python "${SCRIPT_DIR}/tests/run_mission.py" -v
 
 kill $(jobs -p)
