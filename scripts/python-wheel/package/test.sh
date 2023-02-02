42327ef38f031fcad23debeca024a6756ad4b04b
Cassidy Laidlaw <cassidy256@gmail.com>
2023-02-01 21:35:53 -0800

    Add debugging output to look into linux issues

--- scripts/python-wheel/package/test.sh
+++ scripts/python-wheel/package/test.sh
@@ -12,7 +12,7 @@ fi
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 python -m malmo.minecraft launch --timeout 360 &
-until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 1; done
+until [ "$(bash -c 'exec 3<> /dev/tcp/localhost/10000;echo $?' 2>/dev/null)" = "0" ]; do sleep 1; jobs; done
 python "${SCRIPT_DIR}/tests/run_mission.py" -v
 
 trap 'sleep 0' SIGTERM
